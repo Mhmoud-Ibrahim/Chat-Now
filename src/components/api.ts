ebddf061e460a90c62f86b87a039eefb1a334a60
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-02-28 03:26:23 +0400

    ff

--- src/components/api.ts
+++ src/components/api.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'https://chat-server-delta-eight.vercel.app', // رابط الباك اند
+  baseURL: 'https://chat-server-git-main-mahmouds-projects-90220037.vercel.app', // رابط الباك اند
   withCredentials: true, 
 });
 
