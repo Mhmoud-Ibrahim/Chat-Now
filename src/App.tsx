ddcfc78c4a428b6db4f8d71d06ad95e61eab476e
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-02-28 23:23:55 +0400

    ddddddd

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
     {
       path: '', element: <Layout />, children: [
         { index: true, element: <Home/> },
-        { path: 'home', element:<ProtectedRoute><Home/></ProtectedRoute>},
+        { path: 'home', element:<Home/>},
         { path: 'register', element: <Register /> },
         { path: 'login', element: <Login /> },
         { path: 'users', element:<ProtectedRoute><Users/></ProtectedRoute>},
